fcf43d758589434a8be50b2eac179f855b8d2679
Nicolás Parra <nicolas@aureolab.cl>
2021-04-03 18:30:18 -0300

    cambio del main

--- src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-const express= require ('express');
-const authController = require('../controllers/auth.controller');
-const autRouter = express.Router();
-
-autRouter.post('/login',authController.login);
-autRouter.post('/logout',authController.logout);
-// autRouter.post('/login',(req, res)=>{authController.login});
-module.exports = autRouter;
--- src/routes/protected.route.js
+++
@@ -1,7 +0,0 @@
-const express= require('express');
-const protectedRouter = express.Router();
-const userController = require('../controllers/user.controller');
-
-protectedRouter.post('/user/changePassword', userController.changePassword);
-
-module.exports = protectedRouter;
