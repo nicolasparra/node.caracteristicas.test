4122f4e54f8335729651bebb88c5236aeb2f5d78
nicolasparra <nicolas@aureolab.cl>
2021-08-18 13:04:03 -0400

    V1

---
+++ 01 Modulos Core/my-data.js
@@ -0,0 +1,9 @@
+'use strict'
+//Si un dato será privado se utiliza el guión bajo _
+var name = 'Nicolas',
+	email = 'Nicoals@hotmail.com',
+	_phone = '12345678'
+
+module.exports.name = name
+module.exports.email = email
+//module.exports.phone = _phone
